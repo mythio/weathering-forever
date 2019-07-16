22402caaececb5a2c17b5e41eaafb8de8dd61a0e
mythio <mythio.2909@gmail.com>
2019-07-16 09:49:04 +0530

    [fix]: Replace gson with moshi for behaving rudely with kotlin

--- app/src/main/java/com/mythio/weather/network/WeatherApiService.kt
+++ app/src/main/java/com/mythio/weather/network/WeatherApiService.kt
@@ -4,14 +4,14 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.mythio.weather.network.response.WeatherResponse
 import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 private const val BASE_URL = "http://api.apixu.com/v1/"
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(GsonConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create())
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL)
     .build()
